31a6bf68677f6c31fbdbf8c7f8a03bf97d106e32
Xunxin Li <xli9400@Xunxins-MacBook-Pro.local>
2021-03-24 18:53:47 -0400

    Finish 2

--- index.js
+++ index.js
@@ -76,6 +76,26 @@ class Airplane {
   */
   
  class Car {
+    constructor(model,milesPerGallon){
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons){
+      this.tank+=gallons;
+    }
+
+    drive(distance){
+      const noFuels = ((this.tank- (distance/this.milesPerGallon))<=0);
+      if(noFuels){
+        this.odometer+= this.tank * this.milesPerGallon;
+        this.tank = 0;
+        return `I ran out of fuel at ${this.odometer}`;
+      }
+      this.odometer+=distance;
+      this.tank -= distance/this.milesPerGallon;
+    }
     
   }
   
